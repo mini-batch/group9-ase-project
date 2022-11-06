6500e828b2e606c58ed3e13f92c73c25d73a3d4b
David-Batchelder <d.batchelder@btinternet.com>
2022-11-06 12:52:26 +0000

    Updated Logic file organisation and added Shape Logic

--- src/components/PolysphereUI.js
+++ src/components/PolysphereUI.js
@@ -1,8 +1,26 @@
 import React, { useEffect, useState } from "react";
+import { A } from "./Logic/PolysphereLogic/Shapes.js";
+
 
 function Polysphere() {
-    
-    return(null);
+
+    function onLoadButtonClick() {
+        console.log(A.layout);
+    };
+
+    return (
+        <div className="home">
+            <div className="container">
+                <div className="row align-items-center pt-1" id="toprow">
+                    <h1>
+                        Polysphere Puzzle
+                    </h1>
+                    <input type="number" placeholder="Number of squares"></input>
+                    <button type="button" onClick={() => onLoadButtonClick()}>Load</button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Polysphere;
