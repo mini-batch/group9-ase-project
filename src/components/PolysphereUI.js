088bf39f2e2e321378e74f54225ef34fe24e2885
David-Batchelder <d.batchelder@btinternet.com>
2022-11-07 20:49:19 +0000

    Edited PolysphereUI.js to enable testing of solver

--- src/components/PolysphereUI.js
+++ src/components/PolysphereUI.js
@@ -1,13 +1,30 @@
 import React, { useEffect, useState } from "react";
-import { Shape } from "./Logic/PolysphereLogic/Shape.js";
+import { create_dicts } from "./Logic/PolysphereLogic/Create_dict_objects.js";
+import { convert_to_5x11 } from "./Logic/PolysphereLogic/Create_solution.js";
+import { generate_headers, populate_problem_matrix, reduce_problem_matrix } from "./Logic/PolysphereLogic/Generate_problem_matrix.js";
 import { A } from "./Logic/PolysphereLogic/Shapes.js";
 import { solve, sets, items } from "./Logic/PolysphereLogic/Solver.js";
 
 function Polysphere() {
     function onLoadButtonClick() {
-        for (let i of solve(sets, items)) {
-            console.log(i)
+        let start_shapes = ["A", "B", "C", "K"];
+        let start_squares = [["0,0","0,1","0,2","1,0","1,2"],["1,1","2,1","3,1","3,2","4,2"],["4,10","4,9", "3,9", "3,8", "2,9"],["3,7","4,7","4,8"]]
+        let problem_mat = populate_problem_matrix();
+        let problem_def = reduce_problem_matrix(problem_mat, generate_headers(problem_mat), start_shapes, start_squares);
+        problem_mat = problem_def[0];
+        let headers = problem_def[1];
+        let dicts = create_dicts(problem_mat, headers);
+        let count = 0;
+        for (let i of solve(dicts[0], dicts[1])) {
+            console.log(i);
+            console.log(convert_to_5x11(i, problem_mat, headers, start_shapes, start_squares));
+            count += 1;
+            if (count === 10) {
+                //break;
+            }
+            console.log(count);
         }
+        console.log("Fin");
     };
 
     return (
