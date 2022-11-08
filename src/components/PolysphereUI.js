b6af7c5da9dfee3402e093ad961432d5bf51492a
David-Batchelder <d.batchelder@btinternet.com>
2022-11-08 14:55:02 +0000

    Added legend to UI, added shape count and next button for displaying solutions, improved layout

--- src/components/PolysphereUI.js
+++ src/components/PolysphereUI.js
@@ -2,9 +2,9 @@ import React, { useEffect, useState, useRef } from "react";
 import { create_dicts } from "./Logic/PolysphereLogic/Create_dict_objects.js";
 import { convert_to_5x11 } from "./Logic/PolysphereLogic/Create_solution.js";
 import { generate_headers, populate_problem_matrix, reduce_problem_matrix } from "./Logic/PolysphereLogic/Generate_problem_matrix.js";
-import { A } from "./Logic/PolysphereLogic/Shapes.js";
 import { solve, sets, items } from "./Logic/PolysphereLogic/Solver.js";
 import "./PolysphereUI.css";
+import Legend from '../Images/ShapeLegend.png';
 
 function Polysphere() {
     document.body.style.backgroundColor = 'rgba(182, 122, 11, 0.342)';
@@ -12,6 +12,7 @@ function Polysphere() {
     const [outBoard, setOutBoard] = useState(drawOutBoard());
     const stopExecution = useRef(false);
     const [warningIndicator, setWarningIndicator] = useState(false);
+    const [solutionCount, setSolutionCount] = useState(0);
     const [solutions, setSolutions] = useState([]);
 
     const Colours = {
@@ -75,13 +76,10 @@ function Polysphere() {
         for (let i = 0; i < 5; i++) {
             for (let j = 0; j < 11; j++) {
                 let input = document.getElementById("i," + i.toString() + "," + j.toString()).childNodes[0].value;
-                //console.log(input);
                 if (input === "") {
-                    //console.log("Null");
                     continue;
                 }
                 else {
-                    //console.log(input);
                     if (in_shapes.indexOf(input) === -1) {
                         in_shapes.push(input);
                         in_squares.push([i.toString() + "," + j.toString()]);
@@ -110,51 +108,24 @@ function Polysphere() {
 
     function onLoadButtonClick() {
         drawOutBoard();
-        /*
-        let start_shapes = ["A", "B", "C", "K"];
-        let start_squares = [["0,0","0,1","0,2","1,0","1,2"],["1,1","2,1","3,1","3,2","4,2"],["4,10","4,9", "3,9", "3,8", "2,9"],["3,7","4,7","4,8"]]
-        let problem_mat = populate_problem_matrix();
-        let problem_def = reduce_problem_matrix(problem_mat, generate_headers(problem_mat), start_shapes, start_squares);
-        problem_mat = problem_def[0];
-        let headers = problem_def[1];
-        let dicts = create_dicts(problem_mat, headers);
-        let count = 0;
-        for (let i of solve(dicts[0], dicts[1])) {
-            console.log(i);
-            console.log(convert_to_5x11(i, problem_mat, headers, start_shapes, start_squares));
-            count += 1;
-            if (count === 10) {
-                //break;
-            }
-            console.log(count);
-        }
-        console.log("Fin");*/
     };
 
 
-    function onSolveButtonClick(start_shapes_input, start_squares_input) {
+    function onSolveButtonClick() {
+        setSolutionCount(0);
+        setSolutions([]);
         //stopExecution.current = false;
         console.log(convert_inBoard_to_arrays());
         let input = convert_inBoard_to_arrays();
         let input_shapes = input[0];
         let input_squares = input[1];
-        /*if (start_shapes_input !== "") {
-            let start_shapes = start_shapes_input.split(",");
-            let start_squares = start_squares_input.split(",");
-        }        
-        drawPosition([["A","A","A","B","C","E","F","G","H","I","J"],
-                      ["A",0,"A",0,0,0,0,0,0,"L","K"],
-                      [0,0,0,0,0,0,0,0,0,0,0],
-                      [0,0,0,0,0,0,0,0,0,0,0],
-                      [0,0,0,0,0,0,0,0,0,0,0],]);
-        */
         let problem_mat = populate_problem_matrix();
         let problem_def = reduce_problem_matrix(problem_mat, generate_headers(problem_mat), input_shapes, input_squares);
         problem_mat = problem_def[0];
         let headers = problem_def[1];
         let dicts = create_dicts(problem_mat, headers);
         let count = 0;
-        //const solution_gen = solve(dicts[0], dicts[1]);
+        const solution_gen = solve(dicts[0], dicts[1]);
         /*
         while (true) {
             console.log(stopExecution.current);
@@ -167,30 +138,45 @@ function Polysphere() {
             }
             count += 1;
         }*/
-
+        
+        while(true) {
+            let out = solution_gen.next()
+            if (out.done) {
+                break;
+            } else {
+                count += 1;
+                setSolutionCount(count);
+                setSolutions(oldArray => [...oldArray,convert_to_5x11(out.value, problem_mat, headers, input_shapes, input_squares)]);
+            }
+        }
+        /*
         for (let i of solve(dicts[0], dicts[1])) {
+            count += 1;
+            setSolutionCount(count);
+            setSolutions(oldArray => [...oldArray,convert_to_5x11(i, problem_mat, headers, input_shapes, input_squares)]);
             //if (stopExecution.current) {
              //   break;
-            //}
-            console.log(i);
-            console.log(convert_to_5x11(i, problem_mat, headers, input_shapes, input_squares));
-            drawPosition(convert_to_5x11(i, problem_mat, headers, input_shapes, input_squares))
-            count += 1;
+            //}            
             if (count === 10) {
                 //break;
             }
             console.log(count);
-        }
+        }*/
         console.log("Fin");
     };
 
+    function onNextButtonClick() {
+        drawPosition(solutions.pop());
+    }
+
     function onClearButtonClick() {
+        setSolutions([]);
+        setSolutionCount(0);
         clearPosition();
     };
 
     function onStopButtonClick() {
         stopExecution.current = true;
-        console.log(stopExecution.current);
     }
 
     return (
@@ -200,28 +186,25 @@ function Polysphere() {
                     <h1>
                         Polysphere Puzzle
                     </h1>
-                    <form id="positionInputForm">
-                        <p>
-                            Example input: Shape A in top left corner, shape K in top right.<br></br>
-                            Used Shapes = "A, K", Used Squares = "[0,0; 0,1; 0,2; 1,0; 1,2], [0,9; 0,10; 1,10]"
-                        </p>
-                        <input id="usedShapeInputBox" name="usedShapeInputBox" placeholder="Used Shapes" type="text" ></input>
-                        <input id="usedSquaresInputBox" name="usedSquaresInputBox" placeholder="Used Squares" type="text" ></input>
-                        <button type="button" onClick={() => onSolveButtonClick(document.getElementById("usedShapeInputBox").value, document.getElementById("usedSquaresInputBox").value)}>Solve</button>
+                    <form id="positionInputForm" style={{paddingTop:"10px"}}>
+                        <button type="button" onClick={() => onSolveButtonClick()}>Solve</button>
+                        <button type="button" onClick={() => onNextButtonClick()}>Display Next</button>
                         <button type="button" onClick={() => onClearButtonClick()}>Clear</button>
-                        <button type="button" onClick={() => onStopButtonClick()}>Stop</button>
+                        <button type="button" disabled onClick={() => onStopButtonClick()}>Stop</button>
+                        <p>Number of solutions: {solutionCount}</p>
                     </form>
+                </div>
+                <div className="row align-items-center justify-content-center pt-1" id="legend">
+                    <img src={Legend} style={{width:"40%"}}></img>
                 </div>
             </div>
             <div className="container-fluid">
                 <div className="row board">
                     <div className="boardContainer">
-                        {inBoard}
+                            {inBoard}
                     </div>
-                </div>
-                <div className="row board">
                     <div className="boardContainer">
-                        {outBoard}
+                            {outBoard}
                     </div>
                 </div>
             </div>
