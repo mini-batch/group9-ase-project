a0b026d331da7e1422663f9ece06f33549676682
David-Batchelder <d.batchelder@btinternet.com>
2022-11-12 23:06:20 +0000

    Added setup for deliverable 4

---
+++ src/components/PolyPyramidUI.js
@@ -0,0 +1,9 @@
+function PolyPyramid() {
+    return(
+        <div>
+            
+        </div>
+    );
+}
+
+export default PolyPyramid;
--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as Navigation } from "./Navigation";
 export { default as Home } from "./Home";
 export { default as NQueens } from "./NQueensUI";
 export { default as Polysphere } from "./PolysphereUI";
+export { default as PolyPyramid } from "./PolyPyramidUI";
